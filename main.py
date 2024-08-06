902118f64b4817d9b649e0137dc3d37e04b1d5c5
infantdeveloper <128662857+infantdeveloper@users.noreply.github.com>
2024-08-06 02:50:19 +0200

    Bumped yt-dlp version

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_newest_version():
 
 # ------------------------------------  GUI ------------------------------------------------------
 
-version = "1.1.11"
+version = "1.1.12"
 newest_version = get_newest_version()
 
 app = QApplication([])
